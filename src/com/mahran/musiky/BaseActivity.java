416dd6123dbdfa5bca58fbcbfbe5c18c4fc3990a
Abdallah Mahran <eng.abdallah.mahran@gmail.com>
2017-04-01 19:31:30 +0200

    freezing

--- src/com/mahran/musiky/BaseActivity.java
+++ src/com/mahran/musiky/BaseActivity.java
@@ -3,14 +3,14 @@
  */
 package com.mahran.musiky;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * @author Abdallah
  *
  */
-public abstract class BaseActivity extends Activity {
+public abstract class BaseActivity extends FragmentActivity  {
 	protected int activityViewId;
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
